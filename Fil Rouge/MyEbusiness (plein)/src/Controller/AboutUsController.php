aad3ce6bbc366600c9a90db7b37ec4ed695e361c
Alek666666 <alek6.don@gmail.com>
2022-09-21 16:55:04 +0200

    CMD - Courses de chevaux

--- Fil Rouge/MyEbusiness (plein)/src/Controller/AboutUsController.php
+++ Fil Rouge/MyEbusiness (plein)/src/Controller/AboutUsController.php
@@ -2,48 +2,24 @@
 
 namespace App\Controller;
 
-use App\Entity\Cart;
 use App\Data\SearchData;
-use App\Form\SearchType;
-use Doctrine\ORM\EntityManager;
 use App\Service\Cart\CartService;
 use App\Repository\CartRepository;
-use App\Repository\UserRepository;
 use App\Repository\ProductRepository;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\OrderDetailsRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController extends AbstractController
+class AboutUsController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/aboutus', name: 'app_about_us')]
     public function index(?UserInterface $user, CartService $cartService, ProductRepository $productRepository, CategoryRepository $categoryRepository, CartRepository $cartRepository, OrderDetailsRepository $orderDetails, Security $security, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isGranted('ROLE_CLIENT')) {
-            $clientCart = $cartRepository->findOneByUser($user->getId());
-
-            if (!isset($clientCart)) {
-                $clientCart = new Cart();
-                
-                $clientCart->setUser($user);
-                $clientCart->setClientOrderId(strtoupper(uniqid('MUSE::')));
-                $entityManager->persist($clientCart);
-                $entityManager->flush();
-            }
-
-            $cartService->setCart($clientCart);
-            $cartService->setUser($user);
-        }
-        
-    
         $categories = $categoryRepository->findAll();
         $data = new SearchData();
         $products = $productRepository->findSearch($data);
@@ -51,8 +27,7 @@ class HomeController extends AbstractController
         $discount = $productRepository->findDiscount($data);
         $discount2 =$productRepository->findBy(['discount' => true]);
 
-
-        return $this->render('home/index.html.twig', [
+        return $this->render('about_us/index.html.twig', [
             'items'     => $cartService->getFullCart($orderDetails),
             'count'     => $cartService->getItemCount($orderDetails),
             'total' => $cartService->getTotal($orderDetails),

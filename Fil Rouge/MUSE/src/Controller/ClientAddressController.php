a7fa9f2ece790906f6aa9563914686b5dc16bcb0
AlexisDonato <alek6.don@gmail.com>
2022-10-25 09:55:26 +0200

    crud client_address

--- Fil Rouge/MUSE/src/Controller/ClientAddressController.php
+++ Fil Rouge/MUSE/src/Controller/ClientAddressController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\Address;
 use App\Data\SearchData;
-use App\Form\AddressType;
+use App\Form\Address1Type;
 use App\Service\Cart\CartService;
 use App\Repository\UserRepository;
 use App\Repository\AddressRepository;
@@ -19,18 +19,18 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/address')]
-class AddressController extends AbstractController
+#[Route('/client/address')]
+class ClientAddressController extends AbstractController
 {
-    #[Route('/', name: 'app_address_index', methods: ['GET'])]
+    #[Route('/', name: 'app_client_address_index', methods: ['GET'])]
     public function index(UserRepository $userRepository, UserInterface $user, AddressRepository $addressRepository, CartService $cartService, ProductRepository $productRepository, CategoryRepository $categoryRepository, ?OrderDetailsRepository $orderDetails): Response
     {
-        if (!$this->isGranted('ROLE_ADMIN')) {
+        if (!$this->isGranted('ROLE_CLIENT')) {
             $this->addFlash('error', 'Accès refusé');
             return $this->redirectToRoute('login');  
         }
 
-        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+        $this->denyAccessUnlessGranted('ROLE_CLIENT', null, 'User tried to access a page without having ROLE_CLIENT');
 
         $categories = $categoryRepository->findAll();
         $data = new SearchData();
@@ -39,16 +39,11 @@ class AddressController extends AbstractController
         $discount = $productRepository->findDiscount($data);
         $discount2 =$productRepository->findBy(['discount' => true]);
 
+        $addresses = $this->getDoctrine()->getRepository(Address::class)->findByUser($user);
 
-            $addresses = $this->getDoctrine()->getRepository(Address::class)->findByUser($user);
+         $cartService->setUser($user);
 
-        if ($this->isGranted('ROLE_ADMIN')) {
-            $addresses = $addressRepository->findAll();
-        }
-
-        $cartService->setUser($user);
-
-        return $this->render('address/index.html.twig', [
+        return $this->render('client_address/index.html.twig', [
             'items'     => $cartService->getFullCart($orderDetails),
             'count'     => $cartService->getItemCount($orderDetails),
             'total'     => $cartService->getTotal($orderDetails),
@@ -62,54 +57,19 @@ class AddressController extends AbstractController
         ]);
     }
 
-    // #[Route('/by_user_index', name: 'app_address_by_user_index', methods: ['GET'])]
-    // public function index2(UserRepository $userRepository, AddressRepository $addressRepository, CartService $cartService, ProductRepository $productRepository, CategoryRepository $categoryRepository, ?OrderDetailsRepository $orderDetails): Response
-    // {
-    //     if (!$this->isGranted('ROLE_ADMIN')) {
-    //         $this->addFlash('error', 'Accès refusé');
-    //         return $this->redirectToRoute('login');  
-    //     }
 
-    //     $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
-
-    //     $categories = $categoryRepository->findAll();
-    //     $data = new SearchData();
-    //     $products = $productRepository->findSearch($data);
-    //     $products2 =$productRepository->findAll();
-    //     $discount = $productRepository->findDiscount($data);
-    //     $discount2 =$productRepository->findBy(['discount' => true]);
-
-    //     $addresses = $this->getDoctrine()->getRepository(Address::class)->findByUser($user);
-    //     $user = $addresses->getUser();;
-    //     dd($user, $addresses);
-
-
-
-    //     return $this->render('address/by_user_index.html.twig', [
-    //         'items'     => $cartService->getFullCart($orderDetails),
-    //         'count'     => $cartService->getItemCount($orderDetails),
-    //         'total'     => $cartService->getTotal($orderDetails),
-    //         'products'  => $products,
-    //         'products2' => $products2,
-    //         'categories' => $categories,
-    //         'discount'  => $discount,
-    //         'discount2' => $discount2,
-    //         'addresses' => $addresses,
-    //     ]);
-    // }
-
-    #[Route('/new', name: 'app_address_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_client_address_new', methods: ['GET', 'POST'])]
     public function new(Request $request, AddressRepository $addressRepository, CartService $cartService, ProductRepository $productRepository, CategoryRepository $categoryRepository, ?UserInterface $user, ?OrderDetailsRepository $orderDetails, EntityManagerInterface $entityManager): Response
     {
-        if (!$this->isGranted('ROLE_ADMIN')) {
+        if (!$this->isGranted('ROLE_CLIENT')) {
             $this->addFlash('error', 'Accès refusé');
             return $this->redirectToRoute('login');  
         }
 
-        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+        $this->denyAccessUnlessGranted('ROLE_CLIENT', null, 'User tried to access a page without having ROLE_CLIENT');
 
         $address = new Address();
-        $form = $this->createForm(AddressType::class, $address);
+        $form = $this->createForm(Address1Type::class, $address);
         $form->handleRequest($request);
 
         $categories = $categoryRepository->findAll();
@@ -126,26 +86,12 @@ class AddressController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $addressRepository->save($address, true);
 
-            // $address->setName($form->get('name')->getData());
-            // $address->setCountry($form->get('country')->getData());
-            // $address->setZipcode($form->get('zipcode')->getData());
-            // $address->setCity($form->get('city')->getData());
-            // $address->setPathType($form->get('pathType')->getData());
-            // $address->setPathNumber($form->get('pathNumber')->getData());
-            // $address->setBillingAddress($form->get('billingAddress')->getData());
-            // $address->setDeliveryAddress($form->get('deliveryAddress')->getData());
-
-            // $address->setUser($user);
-
-            // $entityManager->persist($address);
-            // $entityManager->flush();
-
-            return $this->redirectToRoute('app_address_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_client_address_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('address/new.html.twig', [
-            'address' => $address,
-            'form' => $form,
+        return $this->renderForm('client_address/new.html.twig', [
+            'address'   => $address,
+            'form'      => $form,
             'items'     => $cartService->getFullCart($orderDetails),
             'count'     => $cartService->getItemCount($orderDetails),
             'total'     => $cartService->getTotal($orderDetails),
@@ -158,15 +104,15 @@ class AddressController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_address_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_client_address_show', methods: ['GET'])]
     public function show(Address $address, CartService $cartService, ProductRepository $productRepository, CategoryRepository $categoryRepository, ?UserInterface $user, ?OrderDetailsRepository $orderDetails): Response
     {
-        if (!$this->isGranted('ROLE_ADMIN')) {
+        if (!$this->isGranted('ROLE_CLIENT')) {
             $this->addFlash('error', 'Accès refusé');
             return $this->redirectToRoute('login');  
         }
 
-        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+        $this->denyAccessUnlessGranted('ROLE_CLIENT', null, 'User tried to access a page without having ROLE_CLIENT');
 
         // The user cannot access other users infos:
         if (!$this->isGranted('ROLE_ADMIN')) {
@@ -185,9 +131,8 @@ class AddressController extends AbstractController
         $discount2 =$productRepository->findBy(['discount' => true]);
 
         $cartService->setUser($user);
-
-        return $this->render('address/show.html.twig', [
-            'address' => $address,
+        return $this->render('client_address/show.html.twig', [
+            'address'   => $address,
             'items'     => $cartService->getFullCart($orderDetails),
             'count'     => $cartService->getItemCount($orderDetails),
             'total'     => $cartService->getTotal($orderDetails),
@@ -200,17 +145,17 @@ class AddressController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_address_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_client_address_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Address $address, AddressRepository $addressRepository, CartService $cartService, ProductRepository $productRepository, CategoryRepository $categoryRepository, ?UserInterface $user, ?OrderDetailsRepository $orderDetails): Response
     {
-        if (!$this->isGranted('ROLE_ADMIN')) {
+        if (!$this->isGranted('ROLE_CLIENT')) {
             $this->addFlash('error', 'Accès refusé');
             return $this->redirectToRoute('login');  
         }
 
-        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+        $this->denyAccessUnlessGranted('ROLE_CLIENT', null, 'User tried to access a page without having ROLE_CLIENT');
 
-        $form = $this->createForm(AddressType::class, $address);
+        $form = $this->createForm(Address1Type::class, $address);
         $form->handleRequest($request);
 
         $categories = $categoryRepository->findAll();
@@ -225,12 +170,13 @@ class AddressController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $addressRepository->save($address, true);
 
-            return $this->redirectToRoute('app_address_index', [], Response::HTTP_SEE_OTHER);
+
+            return $this->redirectToRoute('app_client_address_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('address/edit.html.twig', [
-            'address' => $address,
-            'form' => $form,
+        return $this->renderForm('client_address/edit.html.twig', [
+            'address'   => $address,
+            'form'      => $form,
             'items'     => $cartService->getFullCart($orderDetails),
             'count'     => $cartService->getItemCount($orderDetails),
             'total'     => $cartService->getTotal($orderDetails),
@@ -243,20 +189,20 @@ class AddressController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_address_delete', methods: ['POST'])]
-    public function delete(Request $request, Address $address, AddressRepository $addressRepository, CartService $cartService, ProductRepository $productRepository, CategoryRepository $categoryRepository, ?UserInterface $user, ?OrderDetailsRepository $orderDetails): Response
+    #[Route('/{id}', name: 'app_client_address_delete', methods: ['POST'])]
+    public function delete(Request $request, Address $address, AddressRepository $addressRepository): Response
     {
-        if (!$this->isGranted('ROLE_ADMIN')) {
+        if (!$this->isGranted('ROLE_CLIENT')) {
             $this->addFlash('error', 'Accès refusé');
             return $this->redirectToRoute('login');  
         }
 
-        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+        $this->denyAccessUnlessGranted('ROLE_CLIENT', null, 'User tried to access a page without having ROLE_CLIENT');
 
         if ($this->isCsrfTokenValid('delete'.$address->getId(), $request->request->get('_token'))) {
             $addressRepository->remove($address, true);
         }
 
-        return $this->redirectToRoute('app_address_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_client_address_index', [], Response::HTTP_SEE_OTHER);
     }
 }

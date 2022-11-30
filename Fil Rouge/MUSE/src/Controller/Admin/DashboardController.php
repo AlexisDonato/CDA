4f8bc51f0cbb00edac7eff3b13335074c040fc07
Alek666666 <alek6.don@gmail.com>
2022-11-30 09:40:23 +0100

    adj

--- Fil Rouge/MUSE/src/Controller/Admin/DashboardController.php
+++ Fil Rouge/MUSE/src/Controller/Admin/DashboardController.php
@@ -171,34 +171,13 @@ public function configureUserMenu(UserInterface $user): UserMenu
 
     public function configureAssets(): Assets 
     {
+        // return $assets
+        //         ->addCssFile('css/admin.css');
         return parent::configureAssets();
-            // ->addWebpackEncoreEntry('style');
+
         
     }
-    // Chart.js...
 
-    // private function createChart(): Chart
-    // {
-    //     $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
-    //     $chart->setData([
-    //         'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    //         'datasets' => [
-    //             [
-    //                 'label' => 'My First dataset',
-    //                 'backgroundColor' => 'rgb(255, 99, 132)',
-    //                 'borderColor' => 'rgb(255, 99, 132)',
-    //                 'data' => [0, 10, 5, 2, 20, 30, 45],
-    //             ],
-    //         ],
-    //     ]);
-    //     $chart->setOptions([
-    //         'scales' => [
-    //             'y' => [
-    //                'suggestedMin' => 0,
-    //                'suggestedMax' => 100,
-    //             ],
-    //         ],
-    //     ]);
-    //     return $chart;
-    // }
+
+    // Chart.js doesn't work...
 }

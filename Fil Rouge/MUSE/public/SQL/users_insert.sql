e74e7ff14ca412b29a363fffbea00ddd52585ef1
Alek666666 <alek6.don@gmail.com>
2022-11-30 12:40:06 +0100

    adj

--- Fil Rouge/MUSE/public/SQL/users_insert.sql
+++ Fil Rouge/MUSE/public/SQL/users_insert.sql
@@ -14,14 +14,15 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Listage des données de la table MUSE.user : ~6 rows (environ)
-INSERT INTO `user` (`id`, `email`, `roles`, `password`, `user_name`, `user_lastname`, `birthdate`, `phone_number`, `is_verified`, `register_date`, `vat`, `pro`, `pro_company_name`, `pro_duns`, `pro_job_position`) VALUES
-	(1, 'admin@muse.com', '["ROLE_ADMIN","ROLE_SALES","ROLE_SHIP","ROLE_PRO","ROLE_CLIENT","ROLE_USER"]', '$2y$13$ccySVe3LypY0lq.nZKB1ROQbpKUXCH2fmueDtSsTI.GebmJUmWfkG', 'admin', 'admin', '2022-12-12 00:00:00', '0999999999', 1, '2022-12-12 00:00:00', 0.10, 1, NULL, NULL, NULL),
-	(2, 'sales@muse.com', '["ROLE_SALES","ROLE_SHIP","ROLE_PRO","ROLE_CLIENT","ROLE_USER"]', '$2y$13$2bfOwlTjNtXb4JfVzAxGSOfv9opihXD2KHuHb.NhwhBIQLFNiUeZu', 'sales', 'sales', '2022-12-12 00:00:00', '0999999999', 1, '2022-12-12 00:00:00', 0.10, 1, NULL, NULL, NULL),
-	(3, 'ship@muse.com', '["ROLE_SHIP","ROLE_PRO","ROLE_CLIENT","ROLE_USER"]', '$2y$13$sZPp7tUcYJVwtLJRInOGFe1kdmOgqK.0Yv.rFRvKpGej1uNbgif/a', 'ship', 'ship', '2022-12-12 00:00:00', '0999999999', 1, '2022-12-12 00:00:00', 0.10, 1, NULL, NULL, NULL),
-	(4, 'pro@muse.com', '["ROLE_PRO","ROLE_CLIENT","ROLE_USER"]', '$2y$13$FCBHEF5HE8AR5M2MyBMis.H8OCFRvK3OBY6nXMaEB8q7wMpKDilwi', 'pro', 'pro', '2022-12-12 00:00:00', '0999999999', 1, '2022-12-12 00:00:00', 0.10, 1, NULL, NULL, NULL),
-	(5, 'client@muse.com', '["ROLE_CLIENT","ROLE_USER"]', '$2y$13$cGv5KqaPxeM6BWZHE9224./Wbc/fUaJ7ilYiKZcnZ2aC0DrDg14ee', 'client', 'client', '2022-12-12 00:00:00', '0999999999', 1, '2022-12-12 00:00:00', 0.20, 0, NULL, NULL, NULL),
-	(6, '123@123.com', '["ROLE_CLIENT","ROLE_USER"]', '$2y$13$aXlVF7hE8rT.1tjmUF3eXe2PdoI8j3/YoOkWaI5MrzvF.I.1Hvs/O', 'po', 'po', '2022-11-17 00:00:00', '0999999999', 1, '2022-11-30 10:20:51', 0.20, 0, NULL, NULL, NULL);
+-- Listage des données de la table MUSE.user : ~7 rows (environ)
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `user_name`, `user_lastname`, `birthdate`, `phone_number`, `is_verified`, `register_date`, `vat`, `pro`, `pro_company_name`, `pro_duns`, `pro_job_position`, `agree_terms`) VALUES
+	(1, 'admin@muse.com', '["ROLE_ADMIN","ROLE_SALES","ROLE_SHIP","ROLE_PRO","ROLE_CLIENT","ROLE_USER"]', '$2y$13$ccySVe3LypY0lq.nZKB1ROQbpKUXCH2fmueDtSsTI.GebmJUmWfkG', 'admin', 'admin', '2022-12-12 00:00:00', '0999999999', 1, '2022-12-12 00:00:00', 0.10, 1, NULL, NULL, NULL, 1),
+	(2, 'sales@muse.com', '["ROLE_SALES","ROLE_SHIP","ROLE_PRO","ROLE_CLIENT","ROLE_USER"]', '$2y$13$tvAgwTJWbvYBUOpiyYzXyOsBg4X1jZocwCq5AichRjsHmACQxro3S', 'sales', 'sales', '2022-12-12 00:00:00', '0999999999', 1, '2022-12-12 00:00:00', 0.10, 1, NULL, NULL, NULL, 1),
+	(3, 'ship@muse.com', '["ROLE_SHIP","ROLE_PRO","ROLE_CLIENT","ROLE_USER"]', '$2y$13$wZto0kfDzPviiOga/q5a2uKPXxFZ9G0hOmPKZFlMpvNPcGfd059Iu', 'ship', 'ship', '2022-12-12 00:00:00', '0999999999', 1, '2022-12-12 00:00:00', 0.10, 1, NULL, NULL, NULL, 1),
+	(4, 'pro@muse.com', '["ROLE_PRO","ROLE_CLIENT","ROLE_USER"]', '$2y$13$qFENhSxKWFGae4l/tAApe.bttMTnQatCgEhdpXhFxFW1nUhONYJxe', 'pro', 'pro', '2022-12-12 00:00:00', '0999999999', 1, '2022-12-12 00:00:00', 0.10, 1, NULL, NULL, NULL, 1),
+	(5, 'client@muse.com', '["ROLE_CLIENT","ROLE_USER"]', '$2y$13$cGv5KqaPxeM6BWZHE9224./Wbc/fUaJ7ilYiKZcnZ2aC0DrDg14ee', 'client', 'client', '2022-12-12 00:00:00', '0999999999', 1, '2022-12-12 00:00:00', 0.20, 0, NULL, NULL, NULL, 1),
+	(6, '123@123.com', '["ROLE_CLIENT","ROLE_USER"]', '$2y$13$aXlVF7hE8rT.1tjmUF3eXe2PdoI8j3/YoOkWaI5MrzvF.I.1Hvs/O', 'po', 'po', '2022-11-17 00:00:00', '0999999999', 1, '2022-11-30 10:20:51', 0.20, 0, NULL, NULL, NULL, 1),
+	(9, 'client@client.com', '["ROLE_CLIENT","ROLE_USER"]', '$2y$13$jnHByAAmk7kbw9344XaeVeZuiKNsrDhgmcSSCed1ue/5Oz3EwhV0q', 'Jean-Michel', 'De la Rue', '2022-11-13 00:00:00', '0999999999', 0, '2022-11-30 11:34:43', 0.20, 0, NULL, NULL, NULL, 1);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
